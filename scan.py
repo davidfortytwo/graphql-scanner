f55ade8ce2f5bca87f864ec7fa85ac7e57d50fd1
davidfortytwo <85337431+davidfortytwo@users.noreply.github.com>
2023-05-18 17:12:03 +0200

    Update scan.py
    Add check for zombie objects

--- scan.py
+++ scan.py
@@ -107,6 +107,90 @@ def check_resource_request(url):
             print(colored(f"[-] No excessive resource request vulnerability found at {url}", "green"))
     except Exception as e:
         print(f"Error during resource request check: {e}")
+        
+def check_zombie_objects(url):
+    introspection_query = {
+        'query': '''{
+        __schema {
+            types {
+                name
+                fields {
+                    name
+                    type {
+                        name
+                        kind
+                        ofType {
+                            name
+                            kind
+                        }
+                    }
+                }
+            }
+            queryType {
+                fields {
+                    name
+                    type {
+                        name
+                        kind
+                        ofType {
+                            name
+                            kind
+                        }
+                    }
+                }
+            }
+            mutationType {
+                fields {
+                    name
+                    type {
+                        name
+                        kind
+                        ofType {
+                            name
+                            kind
+                        }
+                    }
+                }
+            }
+            subscriptionType {
+                fields {
+                    name
+                    type {
+                        name
+                        kind
+                        ofType {
+                            name
+                            kind
+                        }
+                    }
+                }
+            }
+        }
+    }'''}
+
+    try:
+        response = requests.post(url, json=introspection_query)
+        response.raise_for_status()
+        response_json = response.json()
+
+        if 'data' in response_json and '__schema' in response_json['data']:
+            types = set(type['name'] for type in response_json['data']['__schema']['types'])
+            query_types = set(field['type']['name'] for field in response_json['data']['__schema']['queryType']['fields'])
+            mutation_types = set(field['type']['name'] for field in response_json['data']['__schema']['mutationType']['fields'])
+            subscription_types = set(field['type']['name'] for field in response_json['data']['__schema']['subscriptionType']['fields'])
+
+            zombie_objects = types - query_types - mutation_types - subscription_types
+
+            if zombie_objects:
+                print(colored(f"[!] Zombie objects found at {url}", "red"))
+                print(colored(f"Typical severity: High", "red"))
+                print("Zombie objects:", ', '.join(zombie_objects))
+            else:
+                print(colored(f"[-] No zombie objects found at {url}", "green"))
+        else:
+            print(colored(f"[-] Introspection is not enabled at {url}", "green"))
+    except Exception as e:
+        print(f"Error during zombie objects check: {e}")        
 
 def check_directive_limit(url):
     directive_query = {
@@ -144,6 +228,7 @@ def main(target):
     introspection_enabled = check_introspection(target)
     if introspection_enabled:
         check_circular_introspection(target)
+        check_zombie_objects(target)
     check_resource_request(target)
     check_directive_limit(target)
     # Call more checks here...
